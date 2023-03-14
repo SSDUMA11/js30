da66abdc10d4910efc1748b8722897a789795e50
SSDUMA11 <dumasofia2@gmail.com>
2023-03-14 12:07:35 +0200

    task_2

--- task_3.js
+++ task_3.js
@@ -4,14 +4,10 @@ const nullLi = document.createElement('li');
 nullLi.textContent = '0';
 nullLi.id = 'null';
 
-const secondLi = document.createElement('li');
-secondLi.textContent = '2';
-secondLi.id = 'second';
-
 const fourthLi = document.createElement('li');
 fourthLi.textContent = '4';
 fourthLi.id = 'fourth';
 
 ul.prepend(nullLi);
 ul.append(fourthLi);
-ul.insertBefore(secondLi, ul.querySelector('#third'));
+ul.querySelector('#third').insertAdjacentHTML('beforebegin', '<li id="second">2</li>');
